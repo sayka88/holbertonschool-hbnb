6f5ed7954813f07c482bbd9110553f44e929c00a
sayka88 <sayka.alekperova@gmail.com>
2024-06-17 06:31:15 +0000

    ipersistence_manager.py

--- persistence/ipersistence_manager.py
+++ persistence/ipersistence_manager.py
@@ -1,83 +1,71 @@
 #!/usr/bin/python3
-# Persistence for users
+# Persistence Interface
 
-from model.user import User
-from persistence.ipersistence_manager import IPersistenceManager
+from abc import ABC, abstractmethod
 
 
-class UserRepository(IPersistenceManager):
-    """Class for managing the persistence of users."""
+class IPersistenceManager(ABC):
+    """Interface for defining persistence manager methods."""
 
-    def __init__(self):
-        """Initializes the UserRepository with an empty
-        dictionary and a next_id counter.
+    @abstractmethod
+    def save(self, entity):
         """
-        self.users = {}
-        self.next_id = 1
-
-    def save(self, user):
-        """
-        Saves a user.
-
-        If the user does not have a user_id, it assigns a new unique ID.
-        The user is then stored in the users dictionary.
-        """
-        if not hasattr(user, 'user_id'):
-            user.user_id = self.next_id
-            self.next_id += 1
-        self.users[user.user_id] = user
-
-    def get(self, user_id):
-        """
-        Fetches a user by its ID.
+        Saves an entity.
 
         Args:
-            user_id (int): The unique identifier of the user.
-
-        Returns:
-            User: The user object if found, otherwise None.
+            entity (object): The entity to be saved.
         """
-        return self.users.get(user_id)
+        pass
 
-    def get_all(self):
+    @abstractmethod
+    def get(self, entity_id):
         """
-        Fetches all users.
-
-        Returns:
-            list: A list of all user objects.
-        """
-        return list(self.users.values())
-
-    def update(self, user_id, new_user_data):
-        """
-        Updates an existing user.
+        Fetches an entity by its ID.
 
         Args:
-            user_id (int): The unique identifier of the user to be updated.
-            new_user_data (dict): A dictionary containing
-            the new data for the user.
+            entity_id (str/int): The unique identifier of the entity.
+
+        Returns:
+            object: The entity object if found, otherwise None.
+        """
+        pass
+
+    @abstractmethod
+    def update(self, entity_id, new_data):
+        """
+        Updates an existing entity.
+
+        Args:
+            entity_id (str/int): The unique identifier of
+            the entity to be updated.
+            new_data (dict): A dictionary containing
+            the new data for the entity.
 
         Returns:
             bool: True if the update was successful, False otherwise.
         """
-        if user_id in self.users:
-            user = self.users[user_id]
-            user.update_user_data(new_user_data)
-            self.save(user)
-            return True
-        return False
+        pass
 
-    def delete(self, user_id):
+    @abstractmethod
+    def delete(self, entity_id):
         """
-        Deletes an existing user.
+        Deletes an existing entity.
 
         Args:
-            user_id (int): The unique identifier of the user to be deleted.
+            entity_id (str/int): The unique identifier
+            of the entity to be deleted.
 
         Returns:
             bool: True if the deletion was successful, False otherwise.
         """
-        if user_id in self.users:
-            del self.users[user_id]
-            return True
-        return False
+        pass
+
+    @abstractmethod
+    def get_all(self):
+        """
+        Fetches all entities.
+
+        Returns:
+            list: A list of all entity objects.
+        """
+        pass
