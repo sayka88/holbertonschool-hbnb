e63bc5793302928e3bea35b0f2333292ebca3c25
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-18 21:46:07 +0400

    Update test_data_manager.py

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -1,55 +1,45 @@
 #!/usr/bin/python3
 
-import os
 import unittest
-import sys
-sys.path.append('..')
+import os
 from data_manager import DataManager
 
 class TestDataManager(unittest.TestCase):
-
     def setUp(self):
-        self.data_manager = DataManager(file_name='test_data.json')
-        # Ensure the test data file is empty before each test
-        with open('test_data.json', 'w') as f:
-            f.write('{}')
+        self.data_manager = DataManager(storage_file='test_data.json')
 
     def tearDown(self):
-        # Remove the test data file after each test
         if os.path.exists('test_data.json'):
             os.remove('test_data.json')
 
     def test_save_entity(self):
-        entity = {'name': 'Test Entity'}
+        entity = {'name': 'Test'}
         saved_entity = self.data_manager.save(entity)
+        self.assertEqual(saved_entity['name'], 'Test')
         self.assertIn('id', saved_entity)
-        self.assertEqual(saved_entity['name'], 'Test Entity')
 
     def test_get_entity(self):
-        entity = {'name': 'Test Entity'}
+        entity = {'name': 'Test'}
         saved_entity = self.data_manager.save(entity)
-        retrieved_entity = self.data_manager.get(saved_entity['id'])
-        self.assertEqual(retrieved_entity, saved_entity)
+        fetched_entity = self.data_manager.get(saved_entity['id'])
+        self.assertEqual(fetched_entity, saved_entity)
 
     def test_update_entity(self):
-        entity = {'name': 'Test Entity'}
+        entity = {'name': 'Test'}
         saved_entity = self.data_manager.save(entity)
-        updated = self.data_manager.update(saved_entity['id'], {'name': 'Updated Entity'})
-        self.assertIsNotNone(updated)
-        retrieved_entity = self.data_manager.get(saved_entity['id'])
-        self.assertEqual(retrieved_entity['name'], 'Updated Entity')
+        self.data_manager.update(saved_entity['id'], {'name': 'Updated Test'})
+        updated_entity = self.data_manager.get(saved_entity['id'])
+        self.assertEqual(updated_entity['name'], 'Updated Test')
 
     def test_delete_entity(self):
-        entity = {'name': 'Test Entity'}
+        entity = {'name': 'Test'}
         saved_entity = self.data_manager.save(entity)
-        deleted = self.data_manager.delete(saved_entity['id'])
-        self.assertTrue(deleted)
-        retrieved_entity = self.data_manager.get(saved_entity['id'])
-        self.assertIsNone(retrieved_entity)
+        self.data_manager.delete(saved_entity['id'])
+        self.assertIsNone(self.data_manager.get(saved_entity['id']))
 
     def test_get_all_entities(self):
-        entity1 = {'name': 'Entity 1'}
-        entity2 = {'name': 'Entity 2'}
+        entity1 = {'name': 'Test1'}
+        entity2 = {'name': 'Test2'}
         self.data_manager.save(entity1)
         self.data_manager.save(entity2)
         all_entities = self.data_manager.get_all()
