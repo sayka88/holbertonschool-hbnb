79a0e5b0b5d1b27860ab13fb62cd03571eb4dc00
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-18 21:54:44 +0400

    Update test_data_manager.py

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -4,6 +4,13 @@ import unittest
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from model.amenity import Amenity
+from model.city import City
+from model.country import Country
+from model.place import Place
+from model.review import Review
+from model.user import User
 from data_manager import DataManager
 
 class TestDataManager(unittest.TestCase):
