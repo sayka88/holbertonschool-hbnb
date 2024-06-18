c99431b73d80e21ed6bfa0337044f343b8c0189b
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-18 21:49:50 +0400

    Update test_data_manager.py

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -3,7 +3,7 @@
 import unittest
 import os
 import sys
-sys.path.append('..')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from data_manager import DataManager
 
 class TestDataManager(unittest.TestCase):
