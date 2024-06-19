3feb76ba60c9dbd6b03ec052089dd6c4a6780437
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-19 21:07:04 +0400

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # data_manager.py
 
-import uuid
-import json
-import os
+import unittest
+from unittest.mock import patch, MagicMock
 import sys
-from unittest.mock import patch
+import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
