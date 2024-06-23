b43fbd15520025b6a692e89a1dd4f02fa70f8d72
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-23 20:53:29 +0400

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
-"""Importing BaseModel Class"""
-from models.base_model import BaseModel
+"""Model for representing amenities."""
 
 
-class Amenity(BaseModel):
-    """Amenity Class"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = kwargs.get('name', '')
+import uuid
+from datetime import datetime
+
+
+class Amenity:
+    """Class representing an amenity."""
+    def __init__(self, name):
+        """Generate a UUID4 for unique identification"""
+        self.amenity_id = str(uuid.uuid4())
+        self.name = name
+        """Record creation timestamp"""
+        self.created_at = datetime.now()
+        """Record update timestamp"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns the amenity data as a dictionary."""
+        return {
+            'amenity_id': self.amenity_id,
+            'name': self.name,
+            """Convert datetime to ISO 8601 format"""
+            'created_at': self.created_at.isoformat(),
+            """Convert datetime to ISO 8601 format"""
+            'updated_at': self.updated_at.isoformat()
+        }
