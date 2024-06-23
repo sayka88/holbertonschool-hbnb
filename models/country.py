2202a3987a2e03340430f90784475ccecdb8a661
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-23 20:54:09 +0400

    Update country.py

--- models/country.py
+++ models/country.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-"""Importing BaseModel class"""
-from models.base_model import BaseModel
+"""Model for representing countries."""
+
+import uuid
+from datetime import datetime
 
 
-class Country(BaseModel):
-    """Country Class"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = kwargs.get('name', '')
+class Country:
+    """Class representing a country."""
+    def __init__(self, name):
+        # Generate a UUID4 for unique identification
+        self.name = name
+        self.country_id = str(uuid.uuid4())
+        self.created_at = datetime.now()  # Record creation timestamp
+        self.updated_at = datetime.now()  # Record update timestamp
+
+    def to_dict(self):
+        """Returns the country data as a dictionary."""
+        return {
+            'country_id': self.country_id,
+            'name': self.name,
+            # Convert datetime to ISO 8601 format
+            'created_at': self.created_at.isoformat(),
+            # Convert datetime to ISO 8601 format
+            'updated_at': self.updated_at.isoformat()
+        }
