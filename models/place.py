3daa9ee8233da705131b0b998d2b4f8a4a02fc73
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-23 20:54:34 +0400

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,21 +1,108 @@
 #!/usr/bin/python3
-"""Importing BaseModel class"""
-from models.base_model import BaseModel
+"""Model for representing places."""
 
 
-class Place(BaseModel):
-    """Class named Place"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = kwargs.get('name', '')
-        self.description = kwargs.get('description', '')
-        self.address = kwargs.get('address', '')
-        self.city_id = kwargs.get('city_id', '')
-        self.latitude = kwargs.get('latitude', 0.0)
-        self.longitude = kwargs.get('longitude', 0.0)
-        self.host_id = kwargs.get('host_id', '')
-        self.number_of_rooms = kwargs.get('number_of_rooms', 0)
-        self.number_of_bathrooms = kwargs.get('number_of_bathrooms', 0)
-        self.price_per_night = kwargs.get('price_per_night', 0.0)
-        self.max_guests = kwargs.get('max_guests', 0)
-        self.amenities = kwargs.get('amenities', [])
+class Place:
+    """Class representing a place."""
+
+    def __init__(
+        self, name, description, address, city_id, latitude, longitude,
+        host_id, number_of_rooms, number_of_bathrooms, price_per_night,
+        max_guests, amenity_ids
+    ):
+        self.place_id = None
+        self.name = name
+        self.description = description
+        self.address = address
+        self.city_id = city_id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.host_id = host_id
+        self.number_of_rooms = number_of_rooms
+        self.number_of_bathrooms = number_of_bathrooms
+        self.price_per_night = price_per_night
+        self.max_guests = max_guests
+        self.amenity_ids = amenity_ids
+        self.reviews = []
+
+    def add_review(self, review):
+        """Adds a review."""
+        self.reviews.append(review)
+
+    def calculate_total_price(self, number_of_nights):
+        """Calculates the total price for a number of nights."""
+        return self.price_per_night * number_of_nights
+
+    def list_amenities(self):
+        """Lists the amenities."""
+        return self.amenity_ids
+
+    def check_availability(self, start_date, end_date):
+        """Checks availability."""
+        pass
+
+    def list_reviews(self):
+        """Lists the reviews."""
+        return self.reviews
+
+    def set_number_of_guests(self, number):
+        """Sets the number of guests."""
+        self.max_guests = number
+
+    def add_description(self, description):
+        """Adds a description."""
+        self.description = description
+
+    def set_number_of_rooms(self, number):
+        """Sets the number of rooms."""
+        self.number_of_rooms = number
+
+    def set_location(self, latitude, longitude):
+        """Sets the location."""
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def add_amenity(self, amenity_id):
+        """Adds an amenity."""
+        self.amenity_ids.append(amenity_id)
+
+    def toggle_availability(self):
+        """Toggles availability."""
+        pass
+
+    def get_description(self):
+        """Gets the description."""
+        return self.description
+
+    def get_location(self):
+        """Gets the location."""
+        return self.latitude, self.longitude
+
+    def update_place_data(self, new_data):
+        """Updates the place data with new data."""
+        for key, value in new_data.items():
+            setattr(self, key, value)
+
+    def delete_amenity(self, amenity_id):
+        """Deletes an amenity from the place by its ID."""
+        if amenity_id in self.amenity_ids:
+            self.amenity_ids.remove(amenity_id)
+
+    def to_dict(self):
+        """Returns the place data as a dictionary."""
+        return {
+            'place_id': self.place_id,
+            'name': self.name,
+            'description': self.description,
+            'address': self.address,
+            'city_id': self.city_id,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'host_id': self.host_id,
+            'number_of_rooms': self.number_of_rooms,
+            'number_of_bathrooms': self.number_of_bathrooms,
+            'price_per_night': self.price_per_night,
+            'max_guests': self.max_guests,
+            'amenity_ids': self.amenity_ids,
+            'reviews': self.reviews
+        }
